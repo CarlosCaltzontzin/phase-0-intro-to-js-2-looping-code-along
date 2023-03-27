9533c17545a1b3bec34922baab62fe1b6430705d
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-03-27 12:30:55 -0500

    completed  looping code along

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = [];
+
+function writeCards(names,event){
+    const msg = [];
+    for(let i = 0; i < names.length; i++){
+        msg[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    };
+    return msg;
+};
+
+function countDown(n){
+    let count = n;
+    while(count >= 0){
+        console.log(count--);
+    };
+};
